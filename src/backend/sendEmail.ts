9bef1b7b3075fc846c8c960fddfc0e70719699bd
shrouxm <mushrouxm@gmail.com>
2024-12-04 13:00:17 -0800

    feat: try not doing it on import

--- src/backend/sendEmail.ts
+++ src/backend/sendEmail.ts
@@ -1,8 +1,8 @@
 import { CreateEmailOptions, Resend } from 'resend';
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function sendEmail(options: CreateEmailOptions) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   const response = await resend.emails.send(options);
 
   if (response.error) {
